bf1ea77a149fff93eb43387fdcd58206f0822c36
senorita_glez <daphnesofiaglez@gmail.com>
2023-09-29 21:28:56 -0600

    jajaja eso no se usaba

--- bordes/global.py
+++ bordes/global.py
@@ -4,7 +4,6 @@ def thresholdingGlobal(D, threshold): # 0 or 255 since the threshold
     upThreshold = []
     downThreshold = [] 
     width_D, height_D = D.shape
-    thresholdImg = np.copy(D)
     for row in range(width_D):
         for column in range(height_D):
             if(D[row][column]>threshold):
