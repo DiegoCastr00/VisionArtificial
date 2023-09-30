3bc4901b62a28955b697577a0bae8ca49e5c5c7f
Diego <diego.castro.elvira@gamil.com>
2023-09-29 21:12:54 -0600

    otsu

---
+++ bordes/otsu.py
@@ -0,0 +1,68 @@
+import numpy as np
+def thresholding(D, threshold):
+    R, G, B = 0, 1, 2
+
+    width_D, height_D= D.shape
+    thresholdImg = np.copy(D)
+    for row in range(width_D):
+        for column in range(height_D):
+            if(D[row][column]>threshold):
+                thresholdImg[row][column] = 0
+            else:
+                thresholdImg[row][column] = 1
+    
+    return thresholdImg
+
+def thresholdOtsu(img): 
+    hist = np.zeros(256)
+    for row in range(img.shape[0]):
+        for column in range(img.shape[1]):
+            hist[img[row][column]] += 1
+    
+    pixelesInImg = img.shape[0]*img.shape[1]
+    WCVList = []
+
+    #Algorithm
+    for threshold in range(len(hist)):
+        #b where hist < threshold
+        b = hist[:threshold]
+        #Weight b
+        w_b = sum(b)/(pixelesInImg)
+        #Mean b
+        m = 0
+        for i in range(threshold):
+            m += (i*hist[i])
+        if(sum(b)==0):
+            m_b = 0
+        else:
+            m_b = m/sum(b)
+        #Variance b
+        v = 0
+        for i in range(threshold):
+            v += ((i-m_b)**2) * hist[i]
+        if(sum(b)==0):
+            v_b = 0
+        else:
+            v_b = v/sum(b)
+
+        #f where hist >= threshold
+        f = hist[threshold:]
+        #Weight f
+        w_f = sum(f)/(pixelesInImg)
+        #Mean f
+        m = 0
+        for i in range(threshold, len(hist)):
+            m += (i*hist[i])
+        m_f = m/sum(f)
+        #Variance f
+        v = 0
+        for i in range(threshold, len(hist)):
+            v += ((i-m_f)**2) * hist[i]
+        v_f = v/sum(f)
+        #Within_class_variance
+        WCV = (w_b*v_b)+(w_f*v_f)
+        WCVList.append(WCV)
+        
+    t = WCVList.index(min(WCVList))
+    imagen = thresholding(img,t)
+    return imagen 
